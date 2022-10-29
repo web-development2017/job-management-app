2e949d840c54a88bed964f635b88d3349bda5ce8
Rich <web_dev2017@outlook.com>
2022-10-29 14:53:27 +0100

    installed react-bootstrap and bootstrap.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Button from 'react-bootstrap/Button';
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <Button variant="primary">Primary</Button>{' '}
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
